4fe31c8c123c5514db1c82cf5df8cc8156b10001
Yanujz <yanujz@live.it>
2026-03-20 16:02:27 +0100

    chore: update Homebrew formula with v0.1.0 sha256 checksums

--- Formula/trep.rb
+++ Formula/trep.rb
@@ -1,42 +1,37 @@
-# This formula is generated automatically by goreleaser on each tagged release.
-# To install the latest release:
+# This formula is maintained in the trep repository.
+# To install:
 #
 #   brew tap Yanujz/trep https://github.com/Yanujz/trep
 #   brew install trep
 #
 # To upgrade:
 #   brew upgrade trep
-#
-# After the first tagged release the sha256 checksums below will be filled in by
-# the release workflow.  Until then, build from source:
-#
-#   go install github.com/trep-dev/trep/cmd/trep@latest
 
 class Trep < Formula
   desc "Convert test results and coverage data to rich self-contained HTML reports"
   homepage "https://github.com/Yanujz/trep"
   license "MIT"
-  version "0.2.0"
+  version "0.1.0"
 
   on_macos do
     on_arm do
       url "https://github.com/Yanujz/trep/releases/download/v#{version}/trep_darwin_arm64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "8ed6f4461f0de34f89e3391d1660cf3aeed13854b69d447fb0f5adecdaf1cab0"
     end
     on_intel do
       url "https://github.com/Yanujz/trep/releases/download/v#{version}/trep_darwin_amd64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "3bd2ff525da64a7d42e0cc5a0f131fdf79e64d199eb9999f818cea651f70c453"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/Yanujz/trep/releases/download/v#{version}/trep_linux_arm64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "dba4ed0f0cdc524b480dce9cac16fc81f43533f7224bc0995d11a5ee30da1a95"
     end
     on_intel do
       url "https://github.com/Yanujz/trep/releases/download/v#{version}/trep_linux_amd64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "56121a6e584110601868702a29652134bada71391730882b4da4134e8a3cb9ff"
     end
   end
 
